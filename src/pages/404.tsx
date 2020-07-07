9a4adfd2f542ac6fe7abb2c6ef27d79cc9d2fe4b
p1t1ch <belobeev.kirill@gmail.com>
2020-07-07 19:41:40 +0300

    setup typescript

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, PageProps } from 'gatsby'
 
-const NotFoundPage = () => {
+const NotFoundPage = ({ location }: PageProps) => {
   return (
     <div>
       <h1>Error 404</h1>
-      <p>Specified path is not defined</p>
+      <p>Path &ldquo;{location.pathname}&rdquo; is not defined</p>
       <Link to="/">Back to index page</Link>
     </div>
   )
