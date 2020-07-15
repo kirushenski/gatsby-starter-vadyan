80dc0cda1db8e33e8d995f67321b8941bc67e14f
p1t1ch <belobeev.kirill@gmail.com>
2020-07-15 10:57:10 +0300

    add focus-visible

--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import 'focus-visible'
 import { ThemeProvider } from 'emotion-theming'
 import { Global } from '@emotion/core'
 import theme from '@/utils/theme'
@@ -11,7 +12,20 @@ interface WrapperProps {
 const Wrapper = ({ children }: WrapperProps) => {
   return (
     <ThemeProvider theme={theme}>
-      <Global styles={fontsCSS} />
+      <Global
+        styles={[
+          ...fontsCSS,
+          {
+            '.js-focus-visible :focus:not(.focus-visible)': {
+              outline: 'none',
+            },
+            ':focus': {
+              outline: `${theme.focus.size}px solid ${theme.focus.color}`,
+              outlineOffset: theme.focus.offset,
+            },
+          },
+        ]}
+      />
       {children}
     </ThemeProvider>
   )
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,11 +1,18 @@
+const colors = {
+  white: '#fff',
+  black: '#000',
+  dark: '#333',
+  brand: '#639',
+}
+
 const theme = {
-  colors: {
-    white: '#fff',
-    black: '#000',
-    dark: '#333',
-    brand: '#639',
-  },
+  colors,
   step: 8,
+  focus: {
+    size: 3,
+    color: colors.black,
+    offset: 2,
+  },
 }
 
 export default theme
