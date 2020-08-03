fda4c9ffc3c2fab5930222cf8964e52e96da2404
p1t1ch <belobeev.kirill@gmail.com>
2020-08-03 23:42:07 +0300

    add e2e tests

---
+++ cypress/e2e/characters.ts
@@ -0,0 +1,8 @@
+describe('characters test', () => {
+  it('characters start with Morty and changed on button click', () => {
+    cy.visit('/showcase/')
+    cy.findByRole('article').contains(/rick/i)
+    cy.findByRole('button', { name: /randomize/i }).click()
+    cy.findByRole('article').contains(/loading/i)
+  })
+})
