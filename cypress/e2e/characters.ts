ba6e045295793cd25f79c8514a3a873670df7a2d
p1t1ch <belobeev.kirill@gmail.com>
2020-08-07 22:07:42 +0300

    remove problematic e2e test

--- cypress/e2e/characters.ts
+++
@@ -1,8 +0,0 @@
-describe('characters test', () => {
-  it('characters start with Rick and changed on button click', () => {
-    cy.visit('/showcase/')
-    cy.findByRole('article').contains(/rick/i)
-    cy.findByRole('button', { name: /randomize/i }).click()
-    cy.findByRole('article').contains(/loading/i)
-  })
-})
